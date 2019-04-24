b8d8513c83e7b9b520b92558baad98dbea5389f9
christinedo <christinevan.do@gmail.com>
2019-04-24 11:36:12 -0700

    add map implementation

--- DIY ADTs/diy_adts.rb
+++ DIY ADTs/diy_adts.rb
@@ -35,3 +35,66 @@ class Queue
     @queue[0]
   end
 end
+
+# Exercise 3 - Map (2D Array)
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    if !self.has(key)
+      @map << [key, value]
+    else
+      @map.each_with_index do |(k, v), i|
+        if k == key
+          @map[i] = [key, value]
+        end
+      end
+    end
+    self
+  end
+
+  def has(key)
+    @map.any? { |k, v| k == key }
+  end
+
+  def get(key)
+    @map.each { |k, v| return v if k == key }
+    nil
+  end
+
+  def delete(key)
+    @map.each_with_index do |(k, v), i|
+      if k == key
+        @map.slice!(i)
+        return true
+      end
+    end
+    false
+  end
+
+  def show
+    @map
+  end
+end
+
+m = Map.new
+p m.show
+m.set(1, "one")
+m.set(2, "two")
+m.set(3, "three")
+m.set(4, "four")
+p m.set(5, "five")
+# p m.show
+puts
+p m.get(3)
+p m.delete(4)
+p m.show
+p m.get(4)
+puts
+p m.has("6")
+p m.has(2)
+puts
+p m.set(2, "TWOOOOO")
+
