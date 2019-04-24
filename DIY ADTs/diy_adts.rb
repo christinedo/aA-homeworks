1edeed35e8bc478bd6a7302d58025bf1c7bf4422
christinedo <christinevan.do@gmail.com>
2019-04-24 10:39:49 -0700

    add gitignore and stack implementation

---
+++ DIY ADTs/diy_adts.rb
@@ -0,0 +1,18 @@
+# Exercise 1 - Stack
+ class Stack
+  def initialize
+    @stack = []
+  end
+
+  def push(el)
+    @stack << el
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack[-1]
+  end
+end
