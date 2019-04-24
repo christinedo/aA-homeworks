7268908d602cf0e2137886dde4f8cd006ea9fd82
christinedo <christinevan.do@gmail.com>
2019-04-24 10:45:46 -0700

    add queue implementation

--- DIY ADTs/diy_adts.rb
+++ DIY ADTs/diy_adts.rb
@@ -1,4 +1,4 @@
-# Exercise 1 - Stack
+# Exercise 1 - Stack (LIFO)
  class Stack
   def initialize
     @stack = []
@@ -14,5 +14,24 @@
 
   def peek
     @stack[-1]
+  end
+end
+
+# Exercise 2 - Queue (FIFO)
+class Queue
+  def initialize
+    @queue = []
+  end
+  
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue[0]
   end
 end
